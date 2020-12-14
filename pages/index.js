3af0b138a875850f868ef976db82f9fe641d8e09
Henrique Barros <henriquexavierb@gmail.com>
2020-12-14 12:18:15 -0300

    test: optimize render

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Head from 'next/head'
-
+import dynamic from 'next/dynamic.js';
 import styled from 'styled-components';
-import NavBar from '../components/NavBar';
-import Resume from '../components/Resume';
-import Preface from '../components/Preface';
-import TimeLine from '../components/TimeLine';
+
+// import NavBar from '../components/NavBar';
+// import Resume from '../components/Resume';
+// import Preface from '../components/Preface';
+// import TimeLine from '../components/TimeLine';
+
+const NavBar = dynamic(() => import('../components/NavBar'));
+const Resume = dynamic(() => import('../components/Resume'));
+const Preface = dynamic(() => import('../components/Preface'));
+const TimeLine = dynamic(() => import('../components/TimeLine'));
 
 const Sections = styled.div`
     width: 100%;
