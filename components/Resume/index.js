692a3cc24c2d319844323b0e4e2c6667faa82714
Henrique Barros <henriquexavierb@gmail.com>
2021-01-15 23:30:32 -0300

    [hb|chore]: update resumo link

--- components/Resume/index.js
+++ components/Resume/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Link } from './styled';
 
 export default function Resume() {
-	const resumePath = process.env.NEXT_PUBLIC_RESUME_LINK;
+	const resumePath = "https://drive.google.com/file/d/1mvGCLa0ST9LcPwNXrmO6r6b1cHglWs4r/view?usp=sharing";
 	
 	return (
 		<Container>
