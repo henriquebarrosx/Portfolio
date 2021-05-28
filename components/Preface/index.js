2e4c0181f447f034759256835cc753c00cc25fe9
Henrique Barros <henriquebarrosx@gmail.com>
2021-05-28 19:15:50 -0300

    Update curriculum link

--- components/Preface/index.js
+++ components/Preface/index.js
@@ -26,7 +26,7 @@ import {
 } from './styled'
 
 export default function Preface() {
-	const curriculumLink = "https://drive.google.com/file/d/1UPjES0-TfuNcEQnZM073OIYLSYXXlujC/view?usp=sharing";
+	const curriculumLink = "https://drive.google.com/file/d/1CRAip6WSlSFK0BW_mw2AVHk2mXM3G6bz/view?usp=sharing";
 
 	const _match = () => {
 		window.open(curriculumLink, '_blank')
